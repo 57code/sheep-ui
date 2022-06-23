312d4d29f17f9f4982ebe936ba735e97a08429da
yt0379 <yt0379@qq.com>
2022-06-23 15:01:03 +0800

    docs: 添加nav菜单

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,5 @@
+import { defineConfig } from 'vitepress'
+
 const sidebar = {
   '/': [
     { text: '快速开始', link: '/', items: [] },
@@ -7,31 +9,42 @@ const sidebar = {
     },
     { text: '导航', items: [] },
     { text: '反馈', items: [] },
-    { text: '数据录入' ,
+    {
+      text: '数据录入',
       items: [{ text: 'Input 输入框', link: '/components/input/' }]
     },
     { text: '数据展示', items: [] },
-    { text: '布局',
-      items:[
-        { text:'Space 间距',link:'/components/space/'}
-      ]
+    {
+      text: '布局',
+      items: [{ text: 'Space 间距', link: '/components/space/' }]
     }
   ]
 }
 
-const config = {
+export default defineConfig({
   themeConfig: {
-    sidebar
+    sidebar,
+    nav: [
+      { text: '向导', link: '/guide' },
+      {
+        text: '课程',
+        items: [
+          { text: 'Vue3组件库开发实战', link: '/course/vue3-comps' },
+          { text: 'Vue3源码全家桶', link: '/course/vue-source' }
+        ]
+      }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/57code/sheep-ui' }
+    ]
   },
   markdown: {
     config(md) {
       // 这里可以使用markdown-it插件
       const { demoBlockPlugin } = require('vitepress-theme-demoblock')
-      md.use(demoBlockPlugin,{
+      md.use(demoBlockPlugin, {
         cssPreprocessor: 'scss'
       })
     }
   }
-}
-
-export default config
+})
