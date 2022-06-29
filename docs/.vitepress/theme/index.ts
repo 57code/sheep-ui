49190bff98969b7151c494b6767c60c36bfb98c8
yuangenbao <769318433@qq.com>
2022-06-30 02:06:40 +0800

    update: docs中的样式调整

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import Theme from 'vitepress/theme'
 import './demo-block.scss'
+import '../../../src/index.scss'
 import {registerComponents} from './register-components'
 import SheepUI from '../../../src/index'
 import type {App} from 'vue'
