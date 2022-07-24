8c85aeba47c7d8d2fd68fba8e0fa40f0afec530c
57code <yt0379@sohu.com>
2022-07-24 17:16:28 +0800

    feat: 样式体系

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import Test from './components/Test'
 import SButton from './button/src/Button'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <test></test>
   <s-button></s-button>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+import './styles/index.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
