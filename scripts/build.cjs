940b6a7a19a6be3f2499d2cb01eb4fccea8e5f7c
57code <yt0379@sohu.com>
2022-07-25 18:25:38 +0800

    feat: 按需打包

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,12 +1,15 @@
+//build.js
 const path = require('path')
+const fsExtra = require('fs-extra')
+// 新增
+const fs = require('fs')
 const { defineConfig, build } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
-const fsExtra = require('fs-extra')
 
-// 入口文件
 const entryFile = path.resolve(__dirname, './entry.ts')
-// 输出目录
+// 组件目录
+const componentsDir = path.resolve(__dirname, '../src')
 const outputDir = path.resolve(__dirname, '../build')
 
 const baseConfig = defineConfig({
@@ -24,13 +27,14 @@ const rollupOptions = {
   }
 }
 
-// 创建package.json文件
-const createPackageJson = () => {
+// 创建时传入包名name
+const createPackageJson = name => {
+  // 根据传入name决定包名、主文件和主模块名称
   const fileStr = `{
-    "name": "sheep-ui",
+    "name": "${name ? name : 'sheep-ui'}",
     "version": "0.0.0",
-    "main": "sheep-ui.umd.js",
-    "module": "sheep-ui.js",
+    "main": "${name ? 'index.umd.js' : 'sheep-ui.umd.js'}",
+    "module": "${name ? 'index.js' : 'sheep-ui.js'}",
     "author": "杨村长",
     "github": "",
     "description": "羊村第一个组件库Sheep-UI，以后村里羊圈能不能建好就看它了！",
@@ -42,13 +46,44 @@ const createPackageJson = () => {
     "license": "ISC",
     "bugs": {
       "url": "https://github.com/57code/sheep-ui/issues"
-    }
+    },
   }`
+  // 存在包名称，给单组件生成package.json文件
+  if (name) {
+    fsExtra.outputFile(
+      path.resolve(outputDir, `${name}/package.json`),
+      fileStr,
+      'utf-8'
+    )
+  } else {
+    fsExtra.outputFile(
+      path.resolve(outputDir, 'package.json'),
+      fileStr,
+      'utf-8'
+    )
+  }
+}
+// 单组件按需构建
+const buildSingle = async name => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        rollupOptions,
+        lib: {
+          entry: path.resolve(componentsDir, name),
+          name: 'index',
+          fileName: 'index',
+          formats: ['es', 'umd']
+        },
+        outDir: path.resolve(outputDir, name)
+      }
+    })
+  )
 
-  fsExtra.outputFile(path.resolve(outputDir, 'package.json'), fileStr, 'utf-8')
+  createPackageJson(name)
 }
 
-//全量构建
 const buildAll = async () => {
   await build(
     defineConfig({
@@ -65,12 +100,23 @@ const buildAll = async () => {
       }
     })
   )
-  // 创建package.json
   createPackageJson()
 }
 
 const buildLib = async () => {
   await buildAll()
+  // 创建单组件包
+  // 获取组件名称组成的数组
+  fs.readdirSync(componentsDir)
+    .filter(name => {
+      // 过滤组件目录：只要目录不要文件，且目录中包含index.ts
+      const componentDir = path.resolve(componentsDir, name)
+      const isDir = fs.lstatSync(componentDir).isDirectory()
+      return isDir && fs.readdirSync(componentDir).includes('index.ts')
+    })
+    .forEach(async name => {
+      await buildSingle(name)
+    })
 }
 
 buildLib()
