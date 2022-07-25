ac02aee6d93e9a24c7e4d8bd14dff15484a861bc
57code <yt0379@sohu.com>
2022-07-25 13:46:12 +0800

    feat：按钮size，disabled，block

--- src/button/src/Button.tsx
+++ src/button/src/Button.tsx
@@ -7,11 +7,14 @@ export default defineComponent({
   name: 'SButton',
   props: buttonProps,
   setup(props: ButtonProps, { slots }) {
-    const { type } = toRefs(props)
-
+    const { type, size, disabled, block } = toRefs(props)
+    const blockClass = block.value ? 's-btn--block' : '' // 新增
     return () => {
       return (
-        <button class={`s-btn s-btn--${type.value}`}>
+        <button
+          class={`s-btn ${blockClass} s-btn--${type.value} s-btn--${size.value}`}
+          disabled={disabled.value}
+        >
           {slots.default ? slots.default() : '按钮'}
         </button>
       )
--- src/button/src/button-types.ts
+++ src/button/src/button-types.ts
@@ -1,11 +1,23 @@
 import { ExtractPropTypes, PropType } from 'vue'
 
 export type IButtonType = 'primary' | 'secondary' | 'text'
-
+export type IButtonSize = 'small' | 'medium' | 'large'
 export const buttonProps = {
   type: {
     type: String as PropType<IButtonType>,
     default: 'secondary'
+  },
+  size: {
+    type: String as PropType<IButtonSize>,
+    default: 'medium'
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  block: {
+    type: Boolean,
+    default: false
   }
 } as const
 
