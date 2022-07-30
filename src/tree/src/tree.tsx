318f21bd531682b3fa2f9e79a5f15a4cf065ac0b
57code <yt0379@sohu.com>
2022-07-30 11:42:02 +0800

    feat: 折叠图标

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -21,6 +21,31 @@ export default defineComponent({
                   paddingLeft: `${24 * (treeNode.level - 1)}px`
                 }}
               >
+                {/* 如果是叶子节点则放一个空白占位元素，否则放一个三角形反馈图标 */}
+                {treeNode.isLeaf ? (
+                  <span
+                    style={{
+                      display: 'inline-block',
+                      width: '25px'
+                    }}
+                  />
+                ) : (
+                  <svg
+                    style={{
+                      width: '25px',
+                      height: '16px',
+                      display: 'inline-block',
+                      transform: treeNode.expanded ? 'rotate(90deg)' : ''
+                    }}
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M384 192v640l384-320.064z"
+                    ></path>
+                  </svg>
+                )}
                 {treeNode.label}
               </div>
             ))
